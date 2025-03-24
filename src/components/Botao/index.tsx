1466005a2d97bcd87b5ff729ed3712c3c237c303
pedro23070020 <pedro23070020@aluno.cesupa.br>
2025-03-24 15:25:20 -0300

    First commit

---
+++ src/components/Botao/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// A classe Botão será tratada como um componente react
+class Botao extends React.Component {
+    render() {
+        return (
+            <button>
+                Botão
+            </button>
+        )
+    }
+}
+
+export default Botao;
